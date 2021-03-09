bdbef5df364930083080a4bafdc65f664733745c
BekkaliyevAssan <bekkaliyevassan@gmail.com>
2021-03-09 11:24:31 +0500

    rn camera is added

--- App.js
+++ App.js
@@ -6,69 +6,30 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import React, {useState} from 'react';
+import {StyleSheet, StatusBar} from 'react-native';
+import {RNCamera} from 'react-native-camera';
 
 const App = () => {
+  // const [readBarcode, setReadBarcode] = useState(true);
+  const barcodeRecognized = (barcode) => {
+    // barcodes.forEach((barcode) => console.warn(barcode.data));
+    // setReadBarcode(false);
+    alert(barcode.data);
+  };
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="white" />
+      <RNCamera
+        style={{
+          flex: 1,
+          width: '100%',
+        }}
+        onBarCodeRead={barcodeRecognized}></RNCamera>
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
